923bf14ba67ca18d85239d347f8dbc55683cc6be
virolivs <103077000+virolivs@users.noreply.github.com>
2022-05-23 11:57:19 -0300

    Update main.js
    fiz mais comentários

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ let xPosition = 500;
 //declarar variável para a posição inicial do personagem no eixo vertical
 let yPosition = 300;
 
-//declarar constante -- para um array contendo ..
+//declarar constante para as setas de direção para um array contendo seus respectivos nomes
 const keysAvaiable = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"]
 
 //declarar constante para as direções tomadas pelo personagem para um array contendo comandos
@@ -37,22 +37,25 @@ window.addEventListener("keydown", (event) => {
         if(character.classList.contains(direction)) character.classList.remove(direction);
     })
 
-
+    //definir condição para utlizar a seta para cima
     if(key === "ArrowUp" && yPosition>0){
         character.classList.add("turnUp");
         yPosition -= VELOCITY;
     }
 
+    //definir condição para utlizar a seta para baixo
     if(key === "ArrowDown" && yPosition < SCREEN_HEIGHT - 100){
         character.classList.add("turnDown");
         yPosition += VELOCITY;
     }
-
+    
+    //definir condição para utlizar a seta para a esquerda
     if(key === "ArrowLeft" && xPosition>0){
         character.classList.add("turnLeft");
         xPosition -= VELOCITY;
     }
-
+    
+    //definir condição para utlizar a seta para a direita
     if(key === "ArrowRight" && xPosition < SCREEN_WIDTH - 100){
         character.classList.add("turnRight");
         xPosition += VELOCITY;
