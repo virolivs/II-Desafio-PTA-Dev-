851d1cd7f063f5e42ce7eb8a59b689a5c8ffbc55
virolivs <103077000+virolivs@users.noreply.github.com>
2022-05-22 21:45:38 -0300

    Primeiros comentários
    Fiz alguns comentários nas primeiras tags

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
+//definir constante identificar o personagem retornar por meio da class
 const character = document.getElementsByClassName("character")[0];
+
+//
 const containerCharacter = document.getElementsByClassName("container-character")[0];
 
-
+//definir constante para a velocidade do personagem
 const VELOCITY = 10;
 
+//
 const SCREEN_WIDTH = screen.width;
 const SCREEN_HEIGHT = screen.height;
 
+//declarar variável para a posição incial do personagem no eixo horizontal
 let xPosition = 500;
+
+//declarar variável para a posição inicial do personagem no eixo vertical
 let yPosition = 300;
 
+//declarar constante -- para um array contendo ..
 const keysAvaiable = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"]
+
+//declarar constante para as direções tomadas pelo personagem para um array contendo comandos
 const directions = ["turnUp", "turnLeft", "turnRight", "turnDown"];
 
+//
 window.addEventListener("keydown", (event) => {
     const key  = event.key;
 
